1f711b1e97a4be234714e19966c6122184ab5f4a
Maryni <37656934+Maryni@users.noreply.github.com>
2022-02-14 16:52:03 +0200

    gin init create Menu scene

---
+++ Assets/Scripts/UIActions.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIActions : MonoBehaviour
+{
+
+    #region public functions
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+
+    #endregion public functions
+
+
+}
